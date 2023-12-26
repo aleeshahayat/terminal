0bfb2f05eb9506b80b8f79d9903134197d9fa64c
aleeshahayat <150888319+aleeshahayat@users.noreply.github.com>
2023-12-26 09:19:56 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your CV</title>
+</head>
+<body>
+    <h1>Your CV</h1>
+    <p>Registration:FA20-BSE-025 #: SP19-BCS/BSE-00</p>
+    <p>Name: UM-E-AIMEN</p>
+    <p>Access the website at: http://localhost:9181</p>
+    <iframe src="cv.html"></iframe>
+</body>
+</html>
